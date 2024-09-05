5d9b8f83050fb881961f59e30499b4fb940e7431
Maminiainaando <hei.maminiaina.444@gmail.com>
2024-09-05 14:18:47 +0100

    fix: user typing,chat

--- script.js
+++ script.js
@@ -1,16 +1,51 @@
-var socket = io();
+document.addEventListener('DOMContentLoaded', fetchUsers);
 const text = document.getElementById("text");
-const typing = document.getElementById("typing");
-const hello = document.getElementById("hello");
-const chat = document.getElementById("chat");
+
 const chatting = document.getElementById("chatting");
 const listuser = document.getElementById("list-user");
 const name=document.getElementById("name");
+const typing = document.getElementById("typing");
+name.innerHTML = localStorage.getItem('username');
+const storedUsername=name.textContent;
+typing.addEventListener("click", () =>{
+    console.log("typing")
+    socket.emit("typing-event", {
+        user: storedUsername
+    })
+})
+const hello = document.getElementById("hello");
+hello.addEventListener("click", () => {
+    console.log('message')
+    socket.emit("message-event", {
+        user: storedUsername,
+        message: text.value
+    });
+    
+})
+const chat = document.getElementById("chat");
+socket.on("message-event", (data) => {
+    const nouveau = document.createElement("span");
+    nouveau.textContent = data.user + " :: " + data.message + " ! ";
+    chat.appendChild(nouveau);
+});
+socket.on("typing-event", (data) => {
+    const notif = document.getElementsByClassName("notif");
+    while (notif.length > 0) {
+        notif[0].remove();
+    }
+    const nouveauParagraphe = document.createElement("p");
+    nouveauParagraphe.classList.add("notif");
+    nouveauParagraphe.textContent = data.user + " is currently typing";
+    
+    chatting.appendChild(nouveauParagraphe);
+});
+
+
 function fetchUsers() {
     fetch('http://localhost:3000/listuser')
-      .then(response => response.json())
-      .then(data => displayUsers(data))
-      .catch(error => console.error('Error:', error));
+        .then(response => response.json())
+        .then(data => displayUsers(data))
+        .catch(error => console.error('Error:', error));
 }
 function displayUsers(users) {
     const listUsers = document.querySelector('#list-users');
@@ -33,43 +68,14 @@ function displayUsers(users) {
 
         const statusSpan = document.createElement('span');
         statusSpan.classList.add('status');
-        // Ajout de la classe en fonction du statut
         statusSpan.classList.add(user.status_user === 'online' || user.status_user === 'active' ? 'online' : 'offline');
         userInfoDiv.appendChild(statusSpan);
 
         listItem.appendChild(userInfoDiv);
         listUsers.appendChild(listItem);
 
-        // Les logs sont utiles pour le débogage
         console.log('User image data:', user.image_user);
         console.log('Image source:', image.src);
     });
 }
 
-document.addEventListener('DOMContentLoaded', fetchUsers);
-
-name.innerHTML = storedUsername;
-typing.addEventListener("click", () => {
-    socket.emit("typing-event", {
-        name: text.value
-    });
-});
-
-hello.addEventListener("click", () => {
-    socket.emit("message-event", {
-        message: text.value
-    });
-});
-
-socket.on("message-event", (data) => {
-    const nouveau = document.createElement("span");
-    nouveau.textContent = storedUsername + " :: " + data.message + " ! ";
-    chat.appendChild(nouveau);
-});
-
-socket.on("typing-event", (data) => {
-    const nouveau = document.createElement("p");
-    nouveau.textContent = storedUsername + " is currently typing";
-    chatting.appendChild(nouveau);
-    localStorage.clear()
-});
--- server.js
+++ server.js
@@ -1,28 +1,34 @@
-const express = require('express');
-const http = require('http');
+const express=require('express');
 const app = express();
+const http = require('http');
 const server = http.createServer(app);
-const { Server } = require("socket.io");
+const {Server} = require("socket.io");
 
-app.use(express.static(__dirname + "/pages"));
 app.use(express.static(__dirname));
 
-const io = new Server(server);
+app.use(express.static(__dirname +"/pages"));
 
-app.get("/", (req, res) => {
+const io =new Server(server);
+
+app.get('/', (req, res)=>{
+
     res.sendFile(__dirname + "/index.html");
+
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
+
+io.on('connection',(socket)=>{
+    console.log('a user connected')
     socket.on("typing-event", (data) => {
-        socket.broadcast.emit("typing-event", data);
+        socket.broadcast.emit("typing-event",data);
+        console.log("typing-event")
     });
     socket.on("message-event", (data) => {
         socket.broadcast.emit("message-event", data);
+        console.log("message-event")
     });
-});
+}) 
 
-server.listen(3001, () => {
-    console.log('Listening on port:3001');
-});
+server.listen(3001,()=>{
+    console.log('Listening on port:3001');  
+})
